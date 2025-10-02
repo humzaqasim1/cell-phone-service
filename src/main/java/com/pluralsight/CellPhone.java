090ee878fb1c7ff235ddcbea48e6af1edae914b4
humza <humzaqasim1@gmail.com>
2025-10-02 13:41:48 -0500

    Created five parameters

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -1,4 +1,67 @@
 package com.pluralsight;
 
 public class CellPhone {
+    private String model;
+    private String phoneNumber;
+    private String serialNumber;
+    private String carrier;
+    private String owner;
+
+    public CellPhone(String phoneNumber, String model, String carrier, String owner, String serialNumber) {
+        this.model = model;
+        this.phoneNumber = phoneNumber;
+        this.serialNumber = serialNumber;
+        this.carrier = carrier;
+        this.owner = owner;
+
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getphoneNumber() {
+        return phoneNumber;
+    }
+    public void dial(String numberBeingDialed) {
+        System.out.println(getOwner() + " is dialing: " + numberBeingDialed);
+    }
+
+
 }
--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,4 +1,23 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CellPhoneApplication {
+    public static void main(String[] args) {
+        CellPhone phone1 = new CellPhone("111", "Iphone", "Verizon", "Dave", "1234567890");
+        CellPhone phone2 = new CellPhone("222", "Galaxy", "AT&T", "Jim", "0987654321");
+        display(phone1);
+        display(phone2);
+        phone1.dial(phone2.getOwner());
+        phone2.dial(phone1.getOwner());
+
+
+    }
+    public static void display(CellPhone phone1) {
+        System.out.println("Phone number is: " + phone1.getphoneNumber() + "\nModel is: " + phone1.getModel() + "\nCarrier is: " + phone1.getCarrier() + "\nOwner is: " + phone1.getOwner() + "\nSerial number is: " + phone1.getSerialNumber());
+
+    }
+
+
+
 }
